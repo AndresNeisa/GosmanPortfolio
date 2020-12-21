0fd3a23ac14dab4075e2a5dcf9c241c339fa5743
Carlos Neisa <cneisa94@gmail.com>
2020-12-21 14:03:39 -0500

    Desktop layout

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", onLoad);
+
+
+function onLoad() {
+    projects = document.querySelectorAll(".projects__link");
+    projects.forEach(element => {
+        element.addEventListener("mouseover", showProjectImage);
+        element.addEventListener("mouseout", removeProjectImage);
+    }
+    );
+}
+
+function showProjectImage() {
+    let container = document.querySelector('.content__img');
+    container.style.backgroundImage = `url("../Images/${this.innerHTML}.jpg`;
+    container.style.opacity = 1;
+    container.style.transform = `scale(1)`;
+}
+
+function removeProjectImage() {
+    let container = document.querySelector('.content__img');
+    container.style.opacity = 0;
+    container.style.transform = `scale(0.5)`;
+}
