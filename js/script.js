e25ac4ffe62d82f5da5717756bbf115fcd00bfb9
Carlos Neisa <cneisa94@gmail.com>
2021-01-10 18:30:28 -0500

    animations version 2

--- js/script.js
+++ js/script.js
@@ -57,9 +57,7 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add("is-visible");
-        }
-        else {
-            entry.target.classList.remove("is-visible");
+            observer.unobserve(entry.target);
         }
     });
 }, options);
